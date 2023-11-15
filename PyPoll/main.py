021168b1cc40a4f18b555b5210cd95ac89b979b6
Amanda Pigman <amandapigman1@gmail.com>
2023-11-15 11:53:50 -0600

    Deleted unnecessary comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,3 @@
-# In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
-# You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". 
-# Your task is to create a Python script that analyzes the votes and calculates each of the following values:
-#  - The total number of votes cast
-#  - A complete list of candidates who received votes
-#  - The percentage of votes each candidate won
-#  - The total number of votes each candidate won
-#  - The winner of the election based on popular vote
-# Your final script should both print the analysis to the terminal and export a text file with the results.
-
 # Modules
 import os
 import csv
@@ -40,7 +30,7 @@ with open(csvpath, 'r') as csvfile:
     print("----------------------------")
     print(" ")
 
-    # Determining the names of the candidates by looping through and 
+    # Determining the names of the candidates by looping through and grabbing the unique values
     candidates = []
     totals = []
     for i in range (0,vote_number):
